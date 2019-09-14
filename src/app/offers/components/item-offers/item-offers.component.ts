fdd5d74f61bde7a0b40ac8765bcdc647e6834b49
william ricardo ariza velez <wilarizav@gmail.com>
2019-09-14 11:10:43 -0500

    nee version

--- src/app/offers/components/item-offers/item-offers.component.ts
+++ src/app/offers/components/item-offers/item-offers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'yilly-item-offers',
+  selector: 'yilly-item-offer',
   templateUrl: './item-offers.component.html',
   styleUrls: ['./item-offers.component.scss']
 })
